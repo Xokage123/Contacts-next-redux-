c84f1765a5fca49f810c13b96288ed9aaed79f4a
kaido <maxartem0419@gmail.com>
2021-09-20 20:18:48 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-
 import { MyLayout } from '../components/layout';
 import { FormContacts } from '../components/form';
-import { getAllContacts } from '../api';
-import { contactsSelector, fetchContacts } from '../slices/contacts';
-
-import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -14,14 +7,4 @@ export default function Home() {
       <FormContacts />
     </MyLayout>
   )
-}
-
-export async function getStaticProps() {
-
-  const contacts = await getAllContacts();
-  return {
-    props: {
-      element: contacts
-    }
-  }
 }
